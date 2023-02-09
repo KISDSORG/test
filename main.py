b575c51e9d1ddebd7676e401e35c9feb57aa4de9
chammmiii <58718071+chammmiii@users.noreply.github.com>
2023-02-09 13:19:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import pickle
+
 import streamlit as st
-# import requests
-# import zipfile
-# import os
-# import pandas as pd
-# from bs4 import BeautifulSoup
+import requests
+import zipfile
+import os
+import pandas as pd
+from bs4 import BeautifulSoup
 import warnings
 
 warnings.filterwarnings(action='ignore')
@@ -12,18 +14,25 @@ headers= {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
           'Accept-Encoding': '*', 'Connection': 'keep-alive'}
 st.set_page_config(layout='wide')
 
+
 # def convert_df(df):
 #     return df.to_csv().encode('utf-8-sig')
 
 
+def get_data():
+    with open('./주식연계채권_최종.pkl', 'rb') as f:
+        df = pickle.load(f)
+
+    return df
+
+
 if __name__ == '__main__':
-          
+
     st.sidebar.title('주식연계채권 발행내역')
 
     knd = st.sidebar.multiselect(
         '채권 종류', ('전환사채권', '신주인수권부사채권', '교환사채권')
     )
-          
     corp_nm = st.sidebar.text_input('발행사명', '삼성전자')
     start_dt = st.sidebar.date_input('시작일')
     end_dt = st.sidebar.date_input('종료일', min_value=start_dt)
@@ -31,7 +40,7 @@ if __name__ == '__main__':
     intr_sf = st.sidebar.slider('만기이자율(%)', 0, 10)
 
     st.write('표면이자율: ', intr_ex, '%')
-    st.write('만기이자율: ', intr_sf, '%') 
+    st.write('만기이자율: ', intr_sf, '%')
 
     if knd == '전환사채권':
         st.write('You selected 전환사채권')
@@ -40,11 +49,8 @@ if __name__ == '__main__':
     else:
         st.write("You selected 교환사채권")
 
-    # st.title('주식연계채권 발행내역 :money_with_wings:')
-    #
-    # corp = st.text_input('기업명', '삼성전자')
-    # start = st.date_input('시작일')
-    # end = st.date_input('종료일', min_value=start)
+    df = get_data()
+    st.dataframe(df)
 
     # button 생성하기
     # if st.button('조회'):
