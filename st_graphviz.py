79c10580e1cfc2f6b64ef4c8b31578eb0709f3a5
chammmiii <58718071+chammmiii@users.noreply.github.com>
2023-03-06 17:25:10 +0900

    Update st_graphviz.py

--- st_graphviz.py
+++ st_graphviz.py
@@ -7,8 +7,6 @@ import matplotlib.font_manager as fm
 from matplotlib import rc
 import pickle
 import warnings
-from pathlib import Path
-import os
 
 # 폰트 관련 세팅
 font_name = fm.FontProperties(fname='./malgun.ttf').get_name()
@@ -115,6 +113,3 @@ else:
 
         st.subheader('[지배구조]')
         st.graphviz_chart(f)
-
-        download_path = str(os.path.join(Path.home(), "Downloads"))
-        f.render(filename='corp_tree', directory=download_path, format='png')
