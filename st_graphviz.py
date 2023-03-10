23c2dc2c39fdb4e261daba6d576958fd76c5baa7
chammmiii <58718071+chammmiii@users.noreply.github.com>
2023-03-10 14:58:08 +0900

    Update st_graphviz.py

--- st_graphviz.py
+++ st_graphviz.py
@@ -93,12 +93,12 @@ if selected == "주식연계채권":
 else:
     st.header("기업 지배구조")
     uploaded_file = st.file_uploader("지배구조 데이터를 업로드 해주세요(확장자:xlsx)", type='xlsx', key="file")
-    
     # 샘플 파일 다운로드
-    with open('./sample.xlsx') as f:
+    with open('./sample.xlsx', encoding='utf-8-sig') as f:
         st.download_button('Sample Input File Download', f)
 
     if uploaded_file is not None:
+
         df = pd.read_excel(uploaded_file)
         # st.dataframe(df)
 
