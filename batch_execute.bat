4f996505cb5327e1188c2b66264d499ded707a53
chanmi.choi <cksanl13@naver.com>
2023-04-17 09:53:52 +0900

    auto push

---
+++ batch_execute.bat
@@ -0,0 +1,1 @@
+"C:\ProgramData\Anaconda3\python.exe" "C:\Users\Administrator\Desktop\ccm\test\batch.py"
---
+++ batch_push.bat
@@ -0,0 +1,9 @@
+cd 'C:\Users\Administrator\Desktop\ccm\test'
+
+git add Mezzanine_bk.pkl
+git add Mezzanine_new.pkl
+git add batch.py
+git commit -m "auto push"
+git push
+
+pause
